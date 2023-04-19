cdacaa23e6ba26756daada53ac4b17564b5eb7ad
KarunAtreya <karunat1999@gmail.com>
2023-04-19 16:49:48 +0545

    added a sentence (third commit by Karun)

--- demoproject/src/App.js
+++ demoproject/src/App.js
@@ -13,6 +13,7 @@ function App() {
           <Paragraph/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
           Edit <code>src/App.js</code> and save something.
         </p>
         <a
