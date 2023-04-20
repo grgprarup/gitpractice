bb8863c30d1b0fcf14e65d79e29d2e4e2cdd10f5
Prarup Gurung <grgprarup@gmail.com>
2023-04-20 09:20:47 +0545

    Removed another line

--- demoproject/src/App.js
+++ demoproject/src/App.js
@@ -13,13 +13,6 @@ function App() {
           <Paragraph/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
       </header>
     </div>
   );
