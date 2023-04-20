56c718854204cf281eeebac4da244791bbe0e929
Prarup Gurung <grgprarup@gmail.com>
2023-04-20 09:16:41 +0545

    Removed line

--- demoproject/src/App.js
+++ demoproject/src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
 
+<<<<<<< HEAD
       <header className="App-header">
         
         <p>
@@ -23,10 +24,32 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn something
+=======
+        <
+        p >
+        Edit < code > src / App.js < /code> and save something. <
+        /p> <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn something yaya <
+          This is a sentence added for the third commit.
+<<<<<<< HEAD
+        /a> <
+        /header> <
+        /div>
+    );
+=======
+>>>>>>> c768d86 (Removed line)
         </a>
       </header>
     </div>
   );
+<<<<<<< HEAD
+=======
+>>>>>>> eba3e56 (Removed line)
+>>>>>>> c768d86 (Removed line)
 }
 
 export default App;
